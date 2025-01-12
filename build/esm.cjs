cdbd8d154ba267eccdbc66a97544f1ccc24d8396
mlogozzo <itslogozzo@gmail.com>
2025-01-12 17:21:58 +1100

    fix: add default exports to esm 'd.ts' output files

--- build/esm.cjs
+++ build/esm.cjs
@@ -8,6 +8,7 @@ const { promisify } = util
 const typeFileExt = '.d.ts'
 const localeDir = path.join(process.env.PWD, 'esm/locale')
 const pluginDir = path.join(process.env.PWD, 'esm/plugin')
+const mainTypePath = path.join(process.env.PWD, 'esm', `index${typeFileExt}`)
 const localeTypePath = path.join(process.env.PWD, 'esm/locale', `index${typeFileExt}`);
 
 (async () => {
@@ -22,8 +23,13 @@ const localeTypePath = path.join(process.env.PWD, 'esm/locale', `index${typeFile
 
     await promisify(ncp)('./types/', './esm')
 
+    const readMainFile = await promisify(fs.readFile)(mainTypePath, 'utf8')
+    const mainResult = readMainFile.replace('export =', 'export default')
+    await promisify(fs.writeFile)(mainTypePath, mainResult, 'utf8')
+
     const readLocaleFile = await promisify(fs.readFile)(localeTypePath, 'utf8')
-    await promisify(fs.writeFile)(localeTypePath, readLocaleFile, 'utf8')
+    const localResult = readLocaleFile.replace('export =', 'export default')
+    await promisify(fs.writeFile)(localeTypePath, localResult, 'utf8')
 
     const readPluginDir = await promisify(fs.readdir)(pluginDir)
     readPluginDir.forEach(async (p) => {
@@ -32,7 +38,8 @@ const localeTypePath = path.join(process.env.PWD, 'esm/locale', `index${typeFile
         const filePath = path.join(pluginDir, p)
         const targetPath = path.join(pluginDir, pluginName, `index${typeFileExt}`)
         const readFile = await promisify(fs.readFile)(filePath, 'utf8')
-        await promisify(fs.writeFile)(targetPath, readFile, 'utf8')
+        const result = readFile.replace('export =', 'export default')
+        await promisify(fs.writeFile)(targetPath, result, 'utf8')
         await promisify(fs.unlink)(filePath)
       }
     })
